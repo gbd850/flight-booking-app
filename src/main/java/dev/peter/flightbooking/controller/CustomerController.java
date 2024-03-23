3726a011d526b83815cb7a13980e6e5c29a5a38c
gbd850 <mmyszaki@gmail.com>
2024-03-23 18:39:49 +0100

    Encapsulate customer logic to return DTO response

--- src/main/java/dev/peter/flightbooking/controller/CustomerController.java
+++ src/main/java/dev/peter/flightbooking/controller/CustomerController.java
@@ -1,7 +1,7 @@
 package dev.peter.flightbooking.controller;
 
 import dev.peter.flightbooking.dto.CustomerRequestDto;
-import dev.peter.flightbooking.model.Customer;
+import dev.peter.flightbooking.dto.CustomerResponseDto;
 import dev.peter.flightbooking.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,17 +16,17 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @GetMapping("{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+    public ResponseEntity<CustomerResponseDto> getCustomerById(@PathVariable Integer id) {
         return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@RequestBody CustomerRequestDto customerRequestDto) {
+    public ResponseEntity<CustomerResponseDto> createCustomer(@RequestBody CustomerRequestDto customerRequestDto) {
         return new ResponseEntity<>(customerService.createCustomer(customerRequestDto), HttpStatus.CREATED);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<Customer> editCustomer(@PathVariable Integer id, @RequestBody CustomerRequestDto customerRequestDto) {
+    public ResponseEntity<CustomerResponseDto> editCustomer(@PathVariable Integer id, @RequestBody CustomerRequestDto customerRequestDto) {
         return new ResponseEntity<>(customerService.editCustomer(id, customerRequestDto), HttpStatus.OK);
     }
 
