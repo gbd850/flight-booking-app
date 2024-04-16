2ab9bbeb1357592d4c514192b021494efefb37fb
gbd850 <mmyszaki@gmail.com>
2024-04-16 23:10:06 +0200

    Add filter result logic

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FlightService } from '../service/flight.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Flight } from '../model/flight';
 import { HttpErrorResponse, HttpEventType } from '@angular/common/http';
+import { FlightRequest } from '../dto/flightRequest';
 
 @Component({
   selector: 'app-search',
@@ -14,22 +15,113 @@ import { HttpErrorResponse, HttpEventType } from '@angular/common/http';
 export class SearchComponent implements OnInit {
   constructor(private flightService: FlightService) {}
 
-  searchData = this.flightService.getSearchData();
+  searchData: FlightRequest|undefined = undefined;
 
-  flights : Flight[] = [];
+  flights: Flight[] = [];
 
   failed = false;
 
   ngOnInit(): void {
+    this.searchData = this.flightService.getSearchData();
+    console.log(this.flightService.getSearchData());
     if (this.searchData === undefined) {
       this.failed = true;
       return;
     }
-    if (this.searchData.startDate === '' || this.searchData.startDate === undefined) {
-      this.flightService.getFlightsByStartLocation(this.searchData.startLocation!).subscribe( {
-        next: (response: Flight[]) => (this.flights = response),
-        error: (error: HttpErrorResponse) => this.failed = true,
+    if (
+      this.searchData.startDate === '' ||
+      this.searchData.startDate === undefined
+    ) {
+      if (
+        this.searchData.endLocation === '' ||
+        this.searchData.endLocation === undefined
+      ) {
+        this.flightService
+          .getFlightsByStartLocation(this.searchData.startLocation!)
+          .subscribe({
+            next: (response: Flight[]) =>
+              (this.flights = [
+                ...new Set(response.map((item) => JSON.stringify(item))),
+              ].map((item) => JSON.parse(item))),
+            error: (error: HttpErrorResponse) => (this.failed = true),
+          });
+        return;
+      }
+      this.flightService
+        .getFlightsByEndLocation(this.searchData.endLocation!)
+        .subscribe({
+          next: (response: Flight[]) =>
+            (this.flights = [
+              ...new Set(
+                [
+                  ...response.filter(
+                    (item) =>
+                      item.startLocation === this.searchData?.startLocation
+                  ),
+                ].map((item) => JSON.stringify(item))
+              ),
+            ].map((item) => JSON.parse(item))),
+          error: (error: HttpErrorResponse) => (this.failed = true),
+        });
+      return;
+    }
+    this.flightService
+      .getFlightsByTimeframe(
+        this.searchData.startDate! as Date,
+        this.searchData.endDate! as Date
+      )
+      .subscribe({
+        next: (response: Flight[]) =>
+          (this.flights = [
+            ...new Set(response.map((item) => JSON.stringify(item))),
+          ].map((item) => JSON.parse(item))),
+        error: (error: HttpErrorResponse) => (this.failed = true),
       });
+    if (
+      this.searchData.startLocation !== '' &&
+      this.searchData.startLocation !== undefined
+    ) {
+      this.flightService
+        .getFlightsByStartLocation(this.searchData.startLocation!)
+        .subscribe({
+          next: (response: Flight[]) =>
+            (this.flights = [
+              ...new Set(
+                [
+                  ...this.flights,
+                  ...response.filter(
+                    (item) =>
+                      item.startDate === this.searchData?.startDate &&
+                      item.endDate === this.searchData.endDate
+                  ),
+                ].map((item) => JSON.stringify(item))
+              ),
+            ].map((item) => JSON.parse(item))),
+          error: (error: HttpErrorResponse) => (this.failed = true),
+        });
+    }
+    if (
+      this.searchData.endLocation !== '' &&
+      this.searchData.endLocation !== undefined
+    ) {
+      this.flightService
+        .getFlightsByEndLocation(this.searchData.endLocation!)
+        .subscribe({
+          next: (response: Flight[]) =>
+            (this.flights = [
+              ...new Set(
+                [
+                  ...this.flights,
+                  ...response.filter(
+                    (item) =>
+                      item.startDate === this.searchData?.startDate &&
+                      item.endDate === this.searchData.endDate
+                  ),
+                ].map((item) => JSON.stringify(item))
+              ),
+            ].map((item) => JSON.parse(item))),
+          error: (error: HttpErrorResponse) => (this.failed = true),
+        });
     }
   }
 }
--- frontend/src/app/service/flight.service.ts
+++ frontend/src/app/service/flight.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Flight } from '../model/flight';
 import { FlightRequest } from '../dto/flightRequest';
 
@@ -16,7 +16,24 @@ export class FlightService {
   constructor(private http: HttpClient) { }
 
   getFlightsByStartLocation(startLocation: string, isFiltered: boolean = false) : Observable<Flight[]> {
+    if (startLocation === undefined) {
+      return of([]);
+    }
     return this.http.get<Flight[]>(`${this.apiUrl}/flight?startLocation=${startLocation}&filterUnavailable=${isFiltered}`);
+  }
+
+  getFlightsByEndLocation(endLocation: string, isFiltered: boolean = false) : Observable<Flight[]> {
+    if (endLocation === undefined) {
+      return of([]);
+    }
+    return this.http.get<Flight[]>(`${this.apiUrl}/flight?endLocation=${endLocation}&filterUnavailable=${isFiltered}`);
+  }
+
+  getFlightsByTimeframe(startDate: Date, endDate: Date, isFiltered: boolean = false) : Observable<Flight[]> {
+    if (startDate === undefined || endDate === undefined) {
+      return of([]);
+    }
+    return this.http.get<Flight[]>(`${this.apiUrl}/flight?startDate=${startDate}&endDate=${endDate}&filterUnavailable=${isFiltered}`);
   }
 
   setSearchData(searchData: FlightRequest) : void {
