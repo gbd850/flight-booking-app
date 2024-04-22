4ed38ea8e911e0191fb4dbd1ba2bdeb5c5d70dc0
gbd850 <mmyszaki@gmail.com>
2024-04-22 20:17:50 +0200

    Add log in logic
    Add login component, create business logic to fetch and store customer token with assigned scopes inside cookies, edit header to dynamically update buttons to log in/sign up or log out

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private cookieService: CookieService) {}
 
+  isLoggedIn$ = new BehaviorSubject(false);
+
+  ngOnInit(): void {
+    if (this.cookieService.check('token')) {
+      this.isLoggedIn$.next(true);
+    } else {
+      this.isLoggedIn$.next(false);
+    }
+  }
 }
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from '../service/customer.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { TokenResponse } from '../dto/TokenResponse';
+import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject, of } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
+
+  loginForm = this.formBuilder.group({
+    login: '',
+    password: '',
+  });
+
+  error$ = new BehaviorSubject(false);
+
+
+  handleErrorResponse(error: HttpErrorResponse) {
+    this.error$.next(true);
+    this.loginForm.reset();
+  }
+
+  handleSuccessfulResponse(token: TokenResponse) {
+    this.error$.next(false);
+    this.cookieService.set('token', token.access_token, token.expires_in);
+    this.cookieService.set('scope', token.scope, token.expires_in);
+    this.router.navigate(['/']);
+  }
+
+  ngOnInit(): void {
+    if (this.cookieService.check('token')) {
+      this.router.navigate(['/']);
+      alert('You are already logged in!');
+    }
+  }
+
+  onSubmit(): void {
+    const login = this.loginForm.value.login!;
+    const password = this.loginForm.value.password!;
+
+    let role: string;
+    this.customerService.getCustomerRole(login).then(
+      (result) => {
+        const scope = result === 'ADMIN' ? 'user.read user.write' : 'user.read';
+        this.customerService.getToken(login, password, scope).subscribe({
+          next: (value: TokenResponse) => this.handleSuccessfulResponse(value),
+          error: (err: HttpErrorResponse) => this.handleErrorResponse(err),
+        });
+      },
+      (err: HttpErrorResponse) => this.handleErrorResponse(err)
+    );
+  }
+}
