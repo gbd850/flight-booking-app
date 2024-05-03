c91776274d90984747e42e23e42ae63230e7e6fa
gbd850 <mmyszaki@gmail.com>
2024-05-03 20:58:02 +0200

    Permit search flights without authentication

--- src/main/java/dev/peter/flightbooking/config/SecurityConfig.java
+++ src/main/java/dev/peter/flightbooking/config/SecurityConfig.java
@@ -23,6 +23,8 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/v1/api/customers/role/**")
                         .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/api/flights")
+                        .permitAll()
                         .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
