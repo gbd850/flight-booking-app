da9eef4cb96575a4d7a742c314c23d3af290c131
gbd850 <mmyszaki@gmail.com>
2024-05-03 20:05:14 +0200

    Add custom claims to OAuth2 access token to expose client id

--- authserver/src/main/java/dev/peter/authserver/config/ApplicationConfig.java
+++ authserver/src/main/java/dev/peter/authserver/config/ApplicationConfig.java
@@ -7,13 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 @Configuration
 public class ApplicationConfig {
 
     @Autowired
     private CustomerRepository customerRepository;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -22,5 +26,16 @@ public class ApplicationConfig {
     @Bean
     RegisteredClientRepository inMemoryRegisteredClientRepository() {
         return new CustomerRegisteredClientRepository(customerRepository);
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer(RegisteredClientRepository registeredClientRepository) {
+        return (context) -> {
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+                context.getClaims().claims((claims) -> {
+                    claims.put("id", registeredClientRepository.findByClientId(claims.get("sub").toString()).getId());
+                });
+            }
+        };
     }
 }
